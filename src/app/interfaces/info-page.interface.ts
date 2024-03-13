607c51220f2c875e6f1dca269c7b181d71786be4
Isaias <chaisystem@gmail.com>
2024-03-12 19:32:11 -0600

    Interfaces in Services

---
+++ src/app/interfaces/info-page.interface.ts
@@ -0,0 +1,11 @@
+export interface InfoPage{
+    titulo?: string;
+    email?: string;
+    nombre_corto?: string;
+    pagina_autor?: string;
+    face?: string;
+    twitter?: string;
+    insta?: string;
+    team?: any[];
+
+}
--- src/app/services/info-page.service.ts
+++ src/app/services/info-page.service.ts
@@ -1,19 +1,20 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { InfoPage } from '../interfaces/info-page.interface';
 
 @Injectable({
   providedIn: 'root'
 })
 export class InfoPageService {
 
-  info: any = {};
+  info: InfoPage = {};
   load = false;
 
   constructor( private http:HttpClient) { 
     console.log("info pagina cargando")
     //leer archivo json
     this.http.get('assets/data/data-page.json')
-      .subscribe(resp => {
+      .subscribe((resp: InfoPage) => {
         this.load = true;
         this.info = resp
         console.log(this.info)
